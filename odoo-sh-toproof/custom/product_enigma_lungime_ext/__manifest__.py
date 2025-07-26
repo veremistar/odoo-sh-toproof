bd5216ce95f7916fbf7046d02f1b41d636494054
Emil Sterea <comenziviking@gmail.com>
2025-07-26 19:53:33 +0300

    Modul Enigma extins: lungime, m², preț automat

---
+++ odoo-sh-toproof/custom/product_enigma_lungime_ext/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Product Enigma Lungime Variabila - Extins",
+    "version": "1.1",
+    "author": "Toproof / Emil Sterea",
+    "category": "Sales",
+    "summary": "Produs cu lungime variabila, calcul suprafata si pret automat in ofertare.",
+    "depends": ["sale", "product"],
+    "data": [],
+    "installable": True,
+    "auto_install": False
+}
