38661559bd170369783a94a5d18aa4dc1c4511cb
Emil Sterea <comenziviking@gmail.com>
2025-07-26 21:15:41 +0300

    Modul verificat Enigma lungime variabilă

---
+++ odoo-sh-toproof/custom/product_enigma_lungime_ext_verified/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Product Enigma Lungime Variabila - Verified",
+    "version": "1.2",
+    "author": "Toproof / Emil Sterea",
+    "license": "LGPL-3",
+    "category": "Sales",
+    "summary": "Produs cu lungime variabila, suprafata si pret calculat automat în ofertare.",
+    "depends": ["sale", "product"],
+    "data": [],
+    "installable": True,
+    "auto_install": False
+}
