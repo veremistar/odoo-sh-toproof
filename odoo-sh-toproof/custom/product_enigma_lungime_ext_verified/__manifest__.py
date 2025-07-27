5959dc3d0735087143e7d3f794f9d27ba0fac7f5
Emil Sterea <comenziviking@gmail.com>
2025-07-27 11:26:13 +0300

    Modul corect: ENIGMA cu lungime variabilă și calcul automat

--- odoo-sh-toproof/custom/product_enigma_lungime_ext_verified/__manifest__.py
+++ odoo-sh-toproof/custom/product_enigma_lungime_ext_verified/__manifest__.py
@@ -1,14 +1,12 @@
 {
-    "name": "Product Enigma Lungime Variabila - Extins",
-    "version": "1.2",
-    "author": "Toproof / Emil Sterea",
-    "license": "LGPL-3",
-    "category": "Sales",
-    "summary": "Produs cu lungime variabilă, suprafață și preț calculat automat în ofertare.",
-    "depends": ["sale", "product"],
-    "data": [
-        "views/product_template_view.xml"
-    ],
-    "installable": True,
-    "auto_install": False
+"name":"Product Enigma Lungime Variabila Extins",
+"version":"1.2",
+"author":"Toproof Emil Sterea",
+"license":"LGPL-3",
+"category":"Sales",
+"summary":"Produs Enigma cu lungime variabila",
+"depends":["sale","product"],
+"data":["/views/product_template_view.xml"],
+"installable":True,
+"auto_install":False
 }
