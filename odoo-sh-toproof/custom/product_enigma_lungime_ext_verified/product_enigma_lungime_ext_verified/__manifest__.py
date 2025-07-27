d5bb183c47e2557a95235a4e7b73e9fe58bbc8f9
Emil Sterea <comenziviking@gmail.com>
2025-07-27 14:53:09 +0300

    Variante Noua cu calculare 4 var

---
+++ odoo-sh-toproof/custom/product_enigma_lungime_ext_verified/product_enigma_lungime_ext_verified/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Product Enigma Lungime Variabilă - Extins",
+    "version": "1.3",
+    "author": "Toproof / Emil Sterea",
+    "license": "LGPL-3",
+    "category": "Sales",
+    "summary": "Produs Enigma cu lungime variabilă și calcul automat al cantității.",
+    "depends": ["sale", "product"],
+    "data": [
+        "views/product_template_view.xml",
+        "views/sale_order_line_view.xml"
+    ],
+    "installable": True,
+    "auto_install": False
+}
---
+++ odoo-sh-toproof/custom/product_enigma_lungime_ext_verified/product_enigma_lungime_ext_verified/models/sale_order_line.py
@@ -0,0 +1,12 @@
+from odoo import models, fields, api
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    lungime_m = fields.Float(string="Lungime (m)")
+
+    @api.onchange('lungime_m', 'product_id', 'product_uom_qty')
+    def _onchange_lungime_m(self):
+        if self.product_id and self.lungime_m and self.product_id.list_price:
+            # latime fixa: 1.176
+            self.price_unit = self.lungime_m * 1.176 * self.product_id.list_price * self.product_uom_qty
