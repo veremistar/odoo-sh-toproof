c7f8aa13c4eecd79e51ce1499f10e3681b3bcfd0
Emil Sterea <comenziviking@gmail.com>
2025-07-27 10:06:38 +0300

    Adăugat view valid XML pentru afișare minimă în formular produs

--- odoo-sh-toproof/custom/product_enigma_lungime_ext/__manifest__.py
+++ odoo-sh-toproof/custom/product_enigma_lungime_ext/__manifest__.py
@@ -1,11 +1,13 @@
-{
+
     "name": "Product Enigma Lungime Variabila - Extins",
     "version": "1.1",
     "author": "Toproof / Emil Sterea",
     "category": "Sales",
     "summary": "Produs cu lungime variabila, calcul suprafata si pret automat in ofertare.",
     "depends": ["sale", "product"],
-    "data": [],
+    "data": [
+    "views/product_template_view.xml"
+],
     "installable": True,
     "auto_install": False
 }
